2bbe8ce3a8fe577446b466bcc1cead1362eb7602
James M Snell <jasnell@gmail.com>
2015-09-19 10:17:21 -0400

    example with v4 features

--- example/lambda.js
+++ example/lambda.js
@@ -1,18 +1,4 @@
-
-//// You can use Passport Strategies for authentication...
-//
-// var BasicStrategy = require('passport-http').BasicStrategy;
-//
-// exports.auth = new BasicStrategy(
-//   function(username, password, done) {
-//     if (username) {
-//       done(null,{username:username});
-//     } else {
-//       done('error');
-//     }
-//   }
-// );
-
-exports.handler = function(input, context) {
-  context.succeed({'a':'b'});
+exports.handler = (input, context) => {
+  setImmediate(()=>context.succeed({'a':'b'}));
 };
+
